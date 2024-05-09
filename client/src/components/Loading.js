c403319c247aba53b23867110dab02d3272bca75
Madalina <madalinaoancea@hotmail.com>
2024-05-08 20:31:42 -0400

    Update spinner

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Oval } from "react-loader-spinner";
 
 function Loading() {
-  return <div>Loading</div>;
+  return (
+    <Oval
+      visible={true}
+      height="50"
+      width="50"
+      color="#612a91"
+      secondaryColor="#c584e0"
+      ariaLabel="oval-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+    />
+  );
 }
 
 export default Loading;
